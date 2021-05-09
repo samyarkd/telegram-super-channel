a825467d8588bfb0a7f92ffa241d79e3eff8aa8d
samyar <75088294+samyarkd@users.noreply.github.com>
2021-05-09 19:31:25 +0430

    Update KEY.py

--- KEY.py
+++ KEY.py
@@ -1,10 +1,4 @@
-api_id = 1482997
-api_hash = '04115d69ede1ded22b48823ad5dadd95'
+api_id = 
+api_hash = ''
 ch_id = ''
 
-x =  [1, 2 , 3]
-
-if 3 not in x:
-    print('ok')
-if 3 in x:
-    print('ok 1')
